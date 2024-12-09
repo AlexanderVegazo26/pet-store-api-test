b099fa921ff4acc11e6c259eddb2ecaefcf39a07
AlexanderVegazo26 <avegazorodriguez@gmail.com>
2024-12-09 00:21:00 -0400

    fixing some paths

---
+++ api-test-automation/src/api/store-api.ts
@@ -0,0 +1,24 @@
+import { Order } from "@/types/store.types";
+import { APIRequestContext, APIResponse } from "@playwright/test";
+
+export class StoreApi {
+  constructor(private request: APIRequestContext) {}
+
+  async getInventory(): Promise<APIResponse> {
+    return await this.request.get("/api/v3/store/inventory");
+  }
+
+  async createOrder(order: Order): Promise<APIResponse> {
+    return await this.request.post("/api/v3/store/order", {
+      data: order,
+    });
+  }
+
+  async getOrderById(orderId: number): Promise<APIResponse> {
+    return await this.request.get(`/api/v3/store/order/${orderId}`);
+  }
+
+  async deleteOrder(orderId: number): Promise<APIResponse> {
+    return await this.request.delete(`/api/v3/store/order/${orderId}`);
+  }
+}
---
+++ api-test-automation/src/api/user-api.ts
@@ -0,0 +1,49 @@
+import { User } from "@/types/user.types";
+import { APIRequestContext, APIResponse } from "@playwright/test";
+
+export class UserApi {
+  constructor(private request: APIRequestContext) {}
+
+  async createUser(user: User): Promise<APIResponse> {
+    return await this.request.post("/api/v3/user", {
+      data: user,
+    });
+  }
+
+  async createUsersWithList(users: User[]): Promise<APIResponse> {
+    return await this.request.post("/api/v3/user/createWithList", {
+      data: users,
+    });
+  }
+
+  async login(username: string, password: string): Promise<APIResponse> {
+    const params = new URLSearchParams({
+      username,
+      password,
+    });
+
+    const queryString = params.toString();
+    return await this.request.get(`/api/v3/user/login?${queryString}`);
+  }
+
+  async logout(): Promise<APIResponse> {
+    return await this.request.get("/api/v3/user/logout");
+  }
+
+  async getUserByUsername(username: string): Promise<APIResponse> {
+    return await this.request.get(`/api/v3/user/${username}`);
+  }
+
+  async updateUser(
+    username: string,
+    userData: Partial<User>
+  ): Promise<APIResponse> {
+    return await this.request.put(`/api/v3/user/${username}`, {
+      data: userData,
+    });
+  }
+
+  async deleteUser(username: string): Promise<APIResponse> {
+    return await this.request.delete(`/api/v3/user/${username}`);
+  }
+}
